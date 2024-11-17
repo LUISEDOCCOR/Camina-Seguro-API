2412c73c51c680fd82d8b97e473d864e1a583c22
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-11-17 13:23:49 -0600

    feat: add field to database

--- database/config.py
+++ database/config.py
@@ -1,4 +1,4 @@
-from peewee import AutoField, CharField, IntegerField, Model, SqliteDatabase
+from peewee import AutoField, CharField, IntegerField, Model, SqliteDatabase, BooleanField
 
 db = SqliteDatabase("./database/database.db")
 
@@ -8,6 +8,7 @@ class Group (Model):
     students = IntegerField(default=0)
     parents = IntegerField(default=0)
     schedule = CharField()
+    isPublic = BooleanField(default=False)
 
     class Meta:
         database = db
